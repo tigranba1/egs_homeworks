c65a8dbc946c82e7daeaf940c4fe9a9056a05222
Tigran <89929732+tigranba1@users.noreply.github.com>
2021-12-14 00:52:59 +0400

    Create Main.java

---
+++ collections/Main.java
@@ -0,0 +1,18 @@
+package com.java.egs.egs_homeworks.collections;
+
+public class Main {
+    public static void main(String[] args) {
+        List arrayList = new ArrayList();
+        arrayList.add("Tt");
+        arrayList.add("Ti");
+        arrayList.add("Tg");
+        arrayList.add("Tr");
+        arrayList.add("Ta");
+        arrayList.add("Tn");
+        arrayList.remove(2);
+        for (int i = 0; i < arrayList.getSize(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+        System.out.println(arrayList.indexOf("Tra"));
+    }
+}
